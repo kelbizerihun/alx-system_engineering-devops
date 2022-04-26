7bf430c5fefe5d5c78d104993c3ad164fb741ea7
kelbizerihun <kelbizerihun3080@gmail.com>
2022-04-26 10:10:06 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
-"""queries the Reddit API, parses the title of all hot articles, and prints a
-sorted count of given keywords(case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
-"""
+''' task 2 module'''
+
 import requests
-import operator
 
 
-def count_words(subreddit, word_list=[], word_dict={}, after=None):
-        headers = {
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-                    'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-                }
-            r = requests.get('https://www.reddit.com/r/{:}/hot.json?after={:}'.format(
-                        subreddit, after), headers=headers, allow_redirects=False)
-                if r.status_code == 200:
-                            json = r.json()
-                                    data_dict = json.get('data')
-                                            post_list = data_dict.get('children')
-                                                    for post in post_list:
-                                                                    post_data_dict = post.get('data')
-                                                                                for word in word_list:
-                                                                                                    title = post_data_dict.get('title').split()
-                                                                                                                    title_copy = []
-                                                                                                                                    for j in title:
-                                                                                                                                                            title_copy.append(j.upper())
-                                                                                                                                                                            count = title_copy.count(word.upper())
-                                                                                                                                                                                            if word_dict.get(word):
-                                                                                                                                                                                                                    word_dict[word] += count
-                                                                                                                                                                                            else:
-                                                                                                                                                                                                                    word_dict[word] = count
-                                                                                                                                                                                                                            after = data_dict.get('after')
-                                                                                                                                                                                                                                    if data_dict.get('after') is None:
-                                                                                                                                                                                                                                                    sorted_list = sorted(word_dict.items(), key=operator.itemgetter(1),
-                                                                                                                                                                                                                                                                                                          reverse=True)
-                                                                                                                                                                                                                                                                for i in range(len(sorted_list) - 1):
-                                                                                                                                                                                                                                                                                    if sorted_list[i][1] == sorted_list[i+1][1]:
-                                                                                                                                                                                                                                                                                                            if sorted_list[i][0] > sorted_list[i+1][0]:
-                                                                                                                                                                                                                                                                                                                                        sorted_list[i], sorted_list[i+1] = sorted_list[
-                                                                                                                                                                                                                                                                                                                                                                        i+1], sorted_list[i]
-                                                                                                                                                                                                                                                                                                                                                    for item in sorted_list:
-                                                                                                                                                                                                                                                                                                                                                                        if item[1] > 0:
-                                                                                                                                                                                                                                                                                                                                                                                                print("{:}: {:}".format(item[0], item[1]))
-                                                                                                                                                                                                                                                                                                                                                                                                            return
-                                                                                                                                                                                                                                                                                                                                                                                                                return count_words(subreddit, word_list, word_dict, after)
-                                                                                                                                                                                                                                                                                                                                                                        else:
-                                                                                                                                                                                                                                                                                                                                                                                    return~
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''return count of keywords in hot posts titles'''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
+    else:
+        return
