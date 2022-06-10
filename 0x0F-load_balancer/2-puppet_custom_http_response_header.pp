9fd80ff54d9d0921719b87a0af9087a6891524ee
kelbizerihun <kelbizerihun3080@gmail.com>
2022-06-10 01:48:08 +0000

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,79 +1,20 @@
-# add stable version of nginx
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+# Configures a brand new Ubuntu machine setting the custom HTTP header
 
-# update software packages list
-exec { 'update packages':
-  command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+  }
 
-# install nginx
 package { 'nginx':
-  ensure     => 'installed',
-}
+  ensure => 'installed',
+    name   => 'nginx',
+    }
 
-# allow HTTP
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+file_line { 'append a line in nginx config file':
+  path  => '/etc/nginx/nginx.conf',
+    line  => "\tadd_header X-Served-By ${hostname};",
+      after => 'http {',
+      }
 
-# change folder rights
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# create index file
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-# create index file
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# add redirection and error page
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content =>
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-               root /var/www/html;
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By \$hostname;
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files \$uri \$uri/ =404;
-        }
-        error_page 404 /404.html;
-        location  /404.html {
-            internal;
-        }
-        
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        }
-}
-",
-}
-# restart nginx
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# start service nginx
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
+exec { 'sudo service nginx restart':
+  command => '/usr/sbin/service nginx restart',
+  }
